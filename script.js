daef8f81fb5653bc4fd5cf6e4ffc64e9e9159e39
Wilson <willythewhale8@gmail.com>
2022-09-20 11:44:34 -0400

    Testing out promises

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
-let input = document.querySelector(".input");
 let definition = document.getElementById("definition")
 
 
 
 
-let renderWord = (callBack)=> {
-    let req = new XMLHttpRequest();
-    req.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`);
-    req.addEventListener('readystatechange', ()=> {
-        if(req.status === 200){
-            console.log(JSON.parse(req.responseText))
-           callBack(req, undefined)
-        }
-        else {
-            callBack(undefined, "Not a word")
-        }
-        
-    })
-
-
-    req.send();
-}
-setInterval(
-    function ()  {renderWord((data, err)=> {
-              
-            if(err){
-                definition.innerText = err
+let renderWord = (aWord) => {
+    return new Promise((resolve, reject) =>{
+        let req = new XMLHttpRequest();
+        req.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${aWord}`);
+        req.addEventListener('readystatechange', ()=> {
+            if(req.status === 200 ){
+                resolve("This is a word")
             }
-            else{
-                let oudata = JSON.parse(data.responseText) 
-                definition.innerText = oudata[0].word
-            }
+            else(
+                reject("Not a word")
+            )
+            
         })
-    },
-     1000
-)
+        req.send();
+    })
+}
 
+
+renderWord("AHHHHHHHHHH").then((word) =>{
+    console.log(word)}
+).catch(nrWord =>{
+    console.log("This is not a real word")
+})
+
+
+// let getSomething = ()=>{
+//     return new Promise((resolved, rejected) =>{
+//         resolved("A word")
+
+//         rejected("Not a word")
+//     })
+// }
+
+
+// getSomething().then(data =>{
+//     console.log(data)
+// })
+
+// let renderWord = () => {
+//     return new Promise((resolved, rejected) =>{
+//         let req = new XMLHttpRequest()
+//         req.open('GET', `https://api.dictionaryapi.dev/api/v2/entries/en/hello`)
+//         req.addEventListener('readystatechange', ()=>{
+//             if(req.status === 200){
+//                 resolved("Resolved!")
+//             }
+//             else{
+//                 rejected("Not Resolved")
+//             }
+//         req.send();
+//         })
+//     })
+// }
+
+// renderWord().then(status =>{
+//     console.log(status)
+// })
