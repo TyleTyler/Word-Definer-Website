a0e7b45f36835684e9ec288570239aec9ec9e8d6
Wilson <willythewhale8@gmail.com>
2022-09-20 14:22:46 -0400

    I got the definitions to work!

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ let input = document.querySelector(".input")
 let renderWord = (aWord) => {
     return new Promise((resolve, reject) =>{
         let req = new XMLHttpRequest();
-        try{ 
-            req.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${aWord}`);}
-        catch(err){
-            console.log("This was not a word")
-        }
+        req.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${aWord}`);
         req.addEventListener('readystatechange', ()=> {
-            if(req.status === 200 ){
-                let word = JSON.parse(req.response)[0].word 
-                resolve(word)
+            if(req.status === 200){
+                try{
+                    let word = JSON.parse(req.response)[0]
+    
+                    resolve(word)
+                }
+                catch(err){
+                    console.log()
+                }
+                
             }
             else(
                 reject("Not a word")
@@ -27,9 +30,17 @@ let renderWord = (aWord) => {
 
 
 input.addEventListener("input",()=>{
+   
     renderWord(input.value).then(word =>{
-        definition.textContent = word   
+        definition.innerText = `${word.word} : `
+        word.meanings.forEach(meaning => {
+             meaning.definitions.forEach(definitions =>{
+                definition.innerHTML += `<br> ${definitions.definition}`
+             })
+        })
 }).catch(ntword => {
         definition.textContent = ntword
     })
 })
+
+
