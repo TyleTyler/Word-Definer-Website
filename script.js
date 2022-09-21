97bbeea2351eeeabc685e6410df772a107cbe584
TylenolSniffinTyler <willythewhale8@gmail.com>
2022-09-20 20:21:38 -0400

    Fixed empty input problem

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-let div = document.querySelector("#word");
+let definition = document.getElementById("definition")
+let input = document.querySelector(".input")
 
 
 
-
-
-let renderWord = (callBack)=> {
-    let req = new XMLHttpRequest();
-    req.open("GET", "https://api.dictionaryapi.dev/api/v2/entries/en/bonjour");
-    req.addEventListener('readystatechange', ()=> {
-        if(req.readyState === 4 && req.status === 200){
-            console.log(JSON.parse(req.responseText))
-           callBack(req)
-        }
+let renderWord = (aWord) => {
+    return new Promise((resolve, reject) =>{
+        let req = new XMLHttpRequest();
+        req.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${aWord}`);
+        req.addEventListener('readystatechange', ()=> {
+            if(req.status === 200){
+                try{
+                    let word = JSON.parse(req.response)[0]
+    
+                    resolve(word)
+                }
+                catch(err){
+                    console.log("error")
+                }
+                
+            }
+            else(
+                reject("Not a word")
+            )
+            
+        })
+        req.send();
     })
-
-
-    req.send();
 }
 
-renderWord((data)=> {
-    let oudata = JSON.parse(data.responseText)
-    div.innerText = oudata[0].word + " " +  oudata[0].meanings[1].definitions[0].definition
+
+input.addEventListener("input",()=>{
+   
+    if(input.value.length === 0){
+        definition.innerText = " "
+    }  else{
+      
+    renderWord(input.value).then(word =>{
+        definition.innerText = `${word.word} : `
+        word.meanings.forEach(meaning => {
+             meaning.definitions.forEach(definitions =>{
+                definition.innerHTML += `<br> ${definitions.definition}`
+             })
+        })
+}).catch(ntword => {
+        definition.textContent = ntword
+    })  
+    }
+
 })
-    
+
 
